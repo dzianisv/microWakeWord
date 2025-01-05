61bc03539707c1a73f227d8cec6228e044457f69
Den <2119348+dzianisv@users.noreply.github.com>
2025-01-05 12:42:49 +0200

    Include demo

--- mic-demo.py
+++ mic-demo.py
@@ -9,14 +9,12 @@ import numpy as np
 logging.basicConfig(level=logging.INFO)
 
 SAMPLE_RATE=16000
-STEP_MS=30
 sliding_window_average_size = 10
 probability_cutoff = 0.5
 
 def require_model():
-    model_filename = "hey_jarvis.tflite"
     model_url = "https://github.com/esphome/micro-wake-word-models/raw/refs/heads/main/models/hey_jarvis.tflite"
-    
+    model_filename = os.path.basename(model_url)
     # Check if the model file exists in the current working directory
     if not os.path.exists(model_filename):
         logging.info(f"{model_filename} not found. Downloading...")
@@ -33,7 +31,6 @@ def capture_audio_and_predict():
     # Initialize PyAudio
     p = pyaudio.PyAudio()
 
-
     # Define audio stream parameters
     stream = p.open(format=pyaudio.paInt16,
                     channels=1,
@@ -41,19 +38,29 @@ def capture_audio_and_predict():
                     input=True,
                     frames_per_buffer=SAMPLE_RATE)
 
+    # Buffer to store the previous 3 samples of audio
+    read_buffer_size = int(SAMPLE_RATE * 0.5)
+    rolling_buffer_size = 3 * read_buffer_size
+    rolling_buffer = np.zeros(rolling_buffer_size, dtype=np.int16)
+
     try:
         while True:
             # Read audio data from the stream
-            read_buffer_size = sliding_window_average_size * SAMPLE_RATE * STEP_MS // 1000
+            
             audio_data = stream.read(read_buffer_size)
             # Convert audio data to numpy array
-            data = np.frombuffer(audio_data, dtype=np.int16)
-            probabilities = model.predict_clip(data, step_ms=30)
-            print(probabilities)
-            avg = sum(probabilities) / len(probabilities)
-            if avg >= probability_cutoff:
-                print("detected")
+            current_data = np.frombuffer(audio_data, dtype=np.int16)
 
+            # Update the rolling buffer with the current data
+            rolling_buffer = np.roll(rolling_buffer, -read_buffer_size)
+            rolling_buffer[-read_buffer_size:] = current_data
+
+            # Predict using the rolling buffer
+            probabilities = model.predict_clip(rolling_buffer)
+            if any(map(lambda p: p > probability_cutoff, probabilities)):
+                rolling_buffer.fill(0)
+                print('detected')
+    
     except KeyboardInterrupt:
         logging.info("Stopping audio capture.")
     finally:
